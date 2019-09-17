7e3a09db53792f4c76d5cdd189c9a3c7b2b03e71
Erica Murdock-Waters <ericahpa@gmail.com>
2019-09-17 17:07:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // Fix the Scope Lab
 
-//declare customerName to be bob in global scope
 var customerName = 'bob';
 
-// modifies the customerName variable
 function upperCaseCustomerName () {
   customerName = customerName.toUpperCase();
 }
